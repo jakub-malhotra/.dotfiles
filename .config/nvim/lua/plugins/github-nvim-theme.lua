060ed58a8d13b4d45ba76190304ffa6268b598bc
ICS4U-1-2024 <ics4u-1-2024@mths.ca>
2024-09-30 08:15:58 -0400

    github dark test

--- .config/nvim/lua/plugins/github-nvim-theme.lua
+++ .config/nvim/lua/plugins/github-nvim-theme.lua
@@ -1,11 +1,9 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "projekt0n/github-nvim-theme",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require("theme").setup({})
+      require("github-theme").setup({})
+      vim.cmd('colorscheme github_dark_default')
     end
-    -- setup must be called before loading
-    vim.cmd.colorscheme "catppuccin"
   }
