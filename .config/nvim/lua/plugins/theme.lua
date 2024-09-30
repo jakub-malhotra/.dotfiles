72c4c01cf47957740f433a12bbd7ff9238b2eaf1
ICS4U-1-2024 <ics4u-1-2024@mths.ca>
2024-09-30 08:15:08 -0400

    test2

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require("theme").setup({})
-      -- setup must be called before loading
-      vim.cmd.colorscheme "catppuccin"
     end
+    -- setup must be called before loading
+    vim.cmd.colorscheme "catppuccin"
   }
