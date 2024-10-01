73a06bf2faae85a418abfdae1185277ea2333c01
ICS4U-1-2024 <ics4u-1-2024@mths.ca>
2024-10-01 11:28:23 -0400

    update

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -17,7 +17,6 @@ vim.keymap.set("i", "kj", "<ESC>", opts)
 
 -- for LSP
 local server_maps = function(bufopts)
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
   end
