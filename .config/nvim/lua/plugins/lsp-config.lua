e6374847d353f5a7e84a13bf02485f64a07cb706
ICS4U-1-2024 <ics4u-1-2024@mths.ca>
2024-10-01 14:18:58 -0400

    update

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,4 +1,4 @@
--- you need to have NPM loaded for the LSP servers, so ensure it it installed
+-- you need to have NPM loaded for the TS LSP server, so ensure it it installed
 -- sudo apt install npm -y
 
 return {
